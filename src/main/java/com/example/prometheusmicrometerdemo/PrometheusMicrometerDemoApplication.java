3bbee921c89f10e919cdbd346a4b5b76e3ec3bae
ikeda-mk <message.to.makoto@gmail.com>
2018-02-26 22:59:41 +0900

    add skelton.

---
+++ src/main/java/com/example/prometheusmicrometerdemo/PrometheusMicrometerDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.prometheusmicrometerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrometheusMicrometerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrometheusMicrometerDemoApplication.class, args);
+	}
+}
